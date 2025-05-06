aa3cb6383f93412ecc256536d6678a978c86cbf8
BumChops <131448422+BumChops@users.noreply.github.com>
2025-05-06 10:27:28 +0100

    Adjusted UI.

--- main.gd
+++ main.gd
@@ -2,28 +2,33 @@ extends Node
 
 @onready var hostButton = $MainUI/VBoxContainer/HostButton
 @onready var joinButton = $MainUI/VBoxContainer/JoinButton
+@onready var mainUI = $MainUI
 
 var codeToJoin = ""
+# Code is 7-char numbers/letters (excluding I and O) --> IPv4 address
 var username = ""
 
-func _on_name_entry_text_changed(newName) -> void:
-	if newName == "":
+func checkInputs():
+	if username == "":
 		hostButton.disabled = true
+	else:
+		hostButton.disabled = false
+	if username == "" or codeToJoin == "":
 		joinButton.disabled = true
 	else:
-		username = newName
-		hostButton.disabled = false
-		if codeToJoin != "":
-			joinButton.disabled = false
+		joinButton.disabled = false
+
+func _on_name_entry_text_changed(newName) -> void:
+	username = newName
+	checkInputs()
 
 func _on_code_entry_text_changed(newCode) -> void:
-	if newCode == "":
-		joinButton.disabled = true
-	elif username != "":
-			joinButton.disabled = false
+	codeToJoin = newCode
+	checkInputs()
 
 func _on_host_button_pressed() -> void:
 	pass # Replace with function body.
+	print("Hosting!")
 
 func _on_join_button_pressed() -> void:
 	pass # Replace with function body.
