8a53cf9c3682a61cd5bc0d3506d32c7663546b5d
BumChops <131448422+BumChops@users.noreply.github.com>
2025-05-07 11:08:36 +0100

    Added base TCP server functionality.

--- main.gd
+++ main.gd
@@ -1,53 +1,106 @@
 extends Node
 
 @onready var hostButton = $MainUI/VBoxContainer/HostButton
+@onready var hostLobbyUI = $HostLobbyUI
 @onready var joinButton = $MainUI/VBoxContainer/JoinButton
+@onready var lobbyCodeDisplay = $HostLobbyUI/VBoxContainer/LobbyCode
 @onready var mainUI = $MainUI
 
-const base34Chars = "0123456789ABCDEFGHJKLMNPQRSTUVWXYZ"
+const PORT = 9080
+var socket = WebSocketPeer.new()
+var tcpServer = null
 
-var codeToJoin = ""
+var clientUp = false
+var serverUp = false
+
+#const base34Chars = "0123456789ABCDEFGHJKLMNPQRSTUVWXYZ"
+
+var joinCode = ""
 # Code is 7-char numbers/letters (excluding I and O) --> IPv4 address
 var hostCode = ""
 var username = ""
 
 func _ready():
-	
 	var http = HTTPRequest.new()
 	add_child(http)
-	http.connect("request_completed", printIP)
+	http.connect("request_completed", getMyCode)
 	http.request("https://api.ipify.org")
 
-func printIP(result, code, header, body):
-	var ipArr = body.get_string_from_utf8().split(".")
-	var ipInt = 0
-	for i in range(len(ipArr)):
-		ipInt += int(ipArr[i]) * (256 ** i)
-	
-	print("Internet IP is: ", ipInt)
-	
+func getMyCode(_r, _c, _h, body):
+	hostCode = body.get_string_from_utf8()
+	lobbyCodeDisplay.text = "Lobby code is: " + hostCode
+	#var ipArr = body.get_string_from_utf8().split(".")
+	#print(ipArr)
+	#var ipInt = 0
+	#for i in range(len(ipArr)):
+	#	ipInt += ipArr[i].to_int() * (256 ** i)
+	#lobbyCodeDisplay.text = str(ipInt)
+
+func _process(_d):
+	manageServer()
+
+func _exit_tree():
+	socket.close()
+	if serverUp:
+		tcpServer.stop()
 
 func checkInputs():
 	if username == "":
 		hostButton.disabled = true
 	else:
 		hostButton.disabled = false
-	if username == "" or codeToJoin == "":
+	if username == "" or joinCode == "":
 		joinButton.disabled = true
 	else:
 		joinButton.disabled = false
+
+func manageServer():
+	if serverUp:
+		while tcpServer.is_connection_available():
+			var connection: StreamPeerTCP = tcpServer.take_connection()
+			assert(connection != null)
+			socket.accept_stream(connection)
+		
+		socket.poll()
+		
+		if socket.get_ready_state() == WebSocketPeer.STATE_OPEN:
+			while socket.get_available_packet_count() != 0:
+				print(socket.get_packet().get_string_from_ascii())
+		
+	elif clientUp:
+		socket.poll()
+
+		if socket.get_ready_state() == WebSocketPeer.STATE_OPEN:
+			while socket.get_available_packet_count():
+				print(socket.get_packet().get_string_from_ascii())
 
 func _on_name_entry_text_changed(newName):
 	username = newName
 	checkInputs()
 
 func _on_code_entry_text_changed(newCode):
-	codeToJoin = newCode
+	joinCode = newCode
 	checkInputs()
 
 func _on_host_button_pressed():
-	pass # Replace with function body.
-	print("Hosting!")
+	mainUI.hide()
+	hostLobbyUI.show()
+	tcpServer = TCPServer.new()
+	if tcpServer.listen(PORT) != OK:
+		print("Cannot start!")
+	else:
+		print("Server started!")
+		serverUp = true
 
 func _on_join_button_pressed():
-	pass # Replace with function body.
+	if socket.connect_to_url("ws://localhost:9080") != OK: #Replace with join code
+		print("Cannot connect!")
+	else:
+		print("Client connected!")
+		clientUp = true
+
+func _on_copy_code_button_pressed():
+	DisplayServer.clipboard_set(hostCode)
+
+func _on_kick_button_pressed():
+	socket.send_text("You have been kicked :(")
